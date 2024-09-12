6e0239deba34d92368c351d60340c46fd149db12
Vishal <vishal112006jain@gmail.com>
2024-09-12 14:50:31 +0530

    COmmit in feature

--- hi.py
+++ hi.py
@@ -1,3 +1,3 @@
 import string
 test= "an apple ,a day : keeps the, doctor away !;"
-test= test.translate(str.maketrans(' ',' ', string.punctuation))
+test= test.translate(str(' ',' ', string.punctuation))
